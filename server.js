8e089c28292ef3490578b3d630fa0c09886569ca
Ritesh Mukim <riteshmukim@gmail.com>
2017-08-20 22:40:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,47 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Ritesh Mukim',
-    heading: 'Article One',
-    date: 'Aug 20, 2017',
-    content: `
-     <div>
-        <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-        </p>
-    </div>
-    <div>
-        <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-        </p>
-    </div>`
+var articles = {
+    'article-one' : {
+        title: 'Article One | Ritesh Mukim',
+        heading: 'Article One',
+        date: 'Aug 20, 2017',
+        content: `
+         <div>
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+        </div>
+        <div>
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+        </div>`
+    },
+    'article-two' : {
+        title: 'Article Two | Ritesh Mukim',
+        heading: 'Article Two',
+        date: 'Aug 20, 2017',
+        content: `
+            <p>
+                This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. 
+            </p>
+            <p>
+                This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. 
+            </p>`
+    },
+    'article-three' : {
+        title: 'Article Two | Ritesh Mukim',
+        heading: 'Article Two',
+        date: 'Aug 20, 2017',
+        content: `
+            <p>
+                This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article.
+            </p>
+            <p>
+                This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article.
+            </p>`
+    }
 };
 
 function createTemplate (data) {
@@ -65,27 +91,20 @@ function createTemplate (data) {
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req, res){
-  res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function(req, res){
+    var articleName =   req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
