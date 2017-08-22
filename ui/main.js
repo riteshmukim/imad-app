e2b4a2ca756337737290a559311bbca80d5536b3
Ritesh Mukim <riteshmukim@gmail.com>
2017-08-23 00:43:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,16 @@ button.onclick = function(){
     
     
 }
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var names = ['name1','name2','name2'];
+    var list = '';
+    for ( vari = 0; i< names.length; i++){
+        list+="<li>"+names[i]+"<li>";
+    }
+    var ul = document.getElementById("namelist");
+    ul.innerHTML = list;
+}
