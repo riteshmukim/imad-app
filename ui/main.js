b06b3e60d41398ef45e7502c9c7daf0a103ad187
Ritesh Mukim <riteshmukim@gmail.com>
2017-08-23 00:15:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,25 @@ var counter = 0;
 
 button.onclick = function(){
     
+    //Create a request
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if ( request.readystate == XMLHttpRequest.Done){
+            //Take some action
+            if( request.status == 200){
+                counter = counter + 1;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not Done Yet
+    };
+    
+    // Make the request
+    request.open('GET','http://riteshmukim.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
 }
